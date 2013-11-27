abebd4e330902fbd1796f2b93860eef94719a1ad
Marion <knight499@gmail.com>
2013-11-26 16:13:20 -0800

    added for choices to iterate over the answer options

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -5,13 +5,50 @@
 // 5. How the heck do we organize our code in a way that's understandable?
 
 
-$.get('/quizzes.json', function(quizzes){
+var Session = {
+  init: function() {
+    this.key = (new Date()).getTime().toString()
+  }
+}
+
+
+function displayQuizzes(quizzes) {
+  // debugger
   for(quiz in quizzes.quizzes){
     quizzes = quizzes.quizzes
-    var clone = $('#quiz').clone()
-    var $quizName = $(clone.text()).text(quizzes[quiz].name)
+    var quizClone = $('#quiz').clone()
+    var $quizName = $(quizClone.text()).text(quizzes[quiz].name)
     var route = "quiz/" + quizzes[quiz].quiz_id
     $quizName.prop('href', route)
     $('.container').append($quizName)
   }
+}
+
+function displayQuestion(questionData) {
+  var question = questionData.question.question
+  // var choices = questionData
+  for(choices in questionData.question.choices){
+    var choice = choice.choice
+  }
+  var questionClone = $('#question').clone()
+  var $questionName = $(questionClone.text()).text(question)
+  debugger
+}
+
+
+$.get('/quizzes.json', displayQuizzes).done( function() {
+   $('a').on('click', function(e){
+      e.preventDefault()
+      var quiz_id = e.currentTarget.href.split("").reverse()[0]
+      var url = "/quizzes/" + quiz_id + "/questions/next.json"
+      var data = Session.key
+      $.ajax({
+        url: url,
+        data: { session_key: data }
+      }).done(displayQuestion)
+    })
+})
+
+$(document).ready(function() {
+  Session.init()
 })
