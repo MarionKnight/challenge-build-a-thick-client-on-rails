b9aadc07939175dc51139837bc087a84ac021b2b
Apprentice <apprentice@dbc24.local>
2013-12-11 22:52:23 -0800

    modularizing to keep session key hidden did not work

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,21 +1,26 @@
-var FetchQuizzes = {
-  params: {
-    url: '/quizzes.json',
-    type: 'get'
-  },
-  listQuizzes: function(data) {
-    $('.container .quiz-list').append("Select from these quizzes:")
-    for (var i = 0; i <= data.quizzes.length; i++) {
-      $('.container').append(data.quizzes[i].quiz_id, " ", data.quizzes[i].name)
+var FetchQuizzes = (function() {
+  var _session_key = null
+  var _params = {
+      url: '/quizzes.json',
+      type: 'get'
+  }
+  return {
+    listQuizzes: function(data) {
+      $('.container .quiz-list').append("Select from these quizzes:")
+      for (var i = 0; i < data.quizzes.length; i++) {
+        console.log(i)
+        $('.container').append(data.quizzes[i].quiz_id, " ", data.quizzes[i].name)
+      }
+      _session_key = "a124f87dec55da23"
     }
   }
-}
+}())
 
 $(function() {
   $('button#get-quizzes').on('click', function(e) {
     e.preventDefault()
     $.ajax(FetchQuizzes.params)
-      .done(FetchQuizzes.listQuizzes)
+      .done(FetchQuizzes.listQuizzes())
   })
   // $('button#quiz').on('click', function(e) {
 
