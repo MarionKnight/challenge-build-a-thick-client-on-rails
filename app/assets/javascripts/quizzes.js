f54fcf8b1e80af1155c8b93b127549c0dd99a442
Apprentice <apprentice@dbc24.local>
2013-12-12 00:04:22 -0800

    working on quiz templating

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,26 +1,32 @@
-var FetchQuizzes = (function() {
-  var _session_key = null
-  var _params = {
-      url: '/quizzes.json',
-      type: 'get'
-  }
-  return {
-    listQuizzes: function(data) {
-      $('.container .quiz-list').append("Select from these quizzes:")
-      for (var i = 0; i < data.quizzes.length; i++) {
-        console.log(i)
-        $('.container').append(data.quizzes[i].quiz_id, " ", data.quizzes[i].name)
-      }
-      _session_key = "a124f87dec55da23"
+var FetchQuizzes = {
+  // session_key: null,
+  params: {
+    url: '/quizzes.json',
+    type: 'get'
+  },
+  listQuizzes: function(data) {
+    var quizTemplate = $.trim($('#quiz_template').html());
+    $('.container .quiz-list').append("Select from these quizzes:")
+    for (var i = 0; i < data.quizzes.length; i++) {
+      var $quiz = $(quizTemplate)
+      $quiz[0].innerText = data.quizzes[i].name
+      console.log($quiz)
+      $('.container').append( $quiz )
+      $('.container').append(
+        data.quizzes[i].quiz_id,
+        " ",
+        data.quizzes[i].name
+      )
     }
   }
-}())
+}
 
 $(function() {
+
   $('button#get-quizzes').on('click', function(e) {
     e.preventDefault()
     $.ajax(FetchQuizzes.params)
-      .done(FetchQuizzes.listQuizzes())
+      .done(FetchQuizzes.listQuizzes)
   })
   // $('button#quiz').on('click', function(e) {
 
